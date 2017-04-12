8fb023ce9f1fb560eddac4a521c05658d6ba076f
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2017-04-12 22:23:02 +0200

    pre final refill code

---
+++ src/arduino/AquaCPU_SMS_EXTENTION/AquaCPU_SMS_SERVICE/AquaCPU_SMS_SERVICE/AquaCPU_SMS_SERVICE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+/*
+RECIEVE CAN MESSAGES WITH A FILTER OR ALL
+
+
+//DOWNLOAD FILTER FILE WITH DATATYPES
+EINE CSV FILE FÜR JEDE EXTENTION MIT DEN MESSAGES
+
+
+
+*/
+int main(int argc, char *argv[])
+{
+	char sz[] = "Hello, World!";	//Hover mouse over "sz" while debugging to see its contents
+	cout << sz << endl;	//<================= Put a breakpoint here
+	return 0;
+}
